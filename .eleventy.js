e2c5a7ad89a0b1f0afbf8d82f7b5607919c468fd
Jason <jasontcrabtree@gmail.com>
2020-04-09 17:13:57 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/script/');
+  eleventyConfig.addPassthroughCopy('./src/assets/');
+
+  eleventyConfig.addPassthroughCopy('json');
+
+  eleventyConfig.addCollection('posts', (collection) => {
+    return collection.getFilteredByGlob('src/posts/*.md');
+  });
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site', //tutorial = dist
+      includes: 'templates', // tutorial = templates
+      data: '_data',
+    },
+    templateFormats: ['html', 'md', 'css'],
+    htmlTemplateEngine: 'liquid',
+    markdownTemplateEngine: 'liquid',
+    passThroughFileCopy: true,
+  };
+};
---
+++ src/posts/welcome.md
@@ -0,0 +1,7 @@
+---
+title: 'This is the first blog post'
+date: '2020-04-08'
+layout: 'layouts/base.html'
+---
+
+Hello world. Welcome **To the jungle.**
